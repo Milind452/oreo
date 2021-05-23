6bcb46c97d940b065983ba0f5bc9d98b9e20335d
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-05-23 19:38:05 +0530

    Create IssueTracker object to validate form data

--- js/app.js
+++ js/app.js
@@ -6,6 +6,30 @@ const passwordField = document.querySelector("#password");
 const loginBtn = document.querySelector("#login");
 const signupBtn = document.querySelector("#signup");
 
+function IssueTracker() {
+    this.issues = [];
+}
+IssueTracker.prototype = {
+    add: function (issue) {
+        this.issues.push(issue);
+    },
+    retrieve: function () {
+        var message = "";
+        switch (this.issues.length) {
+        case 0:
+            // do nothing because message is already ""
+            break;
+        case 1:
+            message = "Please correct the following issue:\n" + this.issues[0];
+            break;
+        default:
+            message = "Please correct the following issues:\n" + this.issues.join("\n");
+            break;
+        }
+        return message;
+    }
+};
+
 loginBtn.addEventListener('click', e => {
     e.preventDefault();
     const nameFieldClassList = nameFieldDiv.classList
