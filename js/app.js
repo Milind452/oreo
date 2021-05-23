2c82bdb34e76f3eb54e0e7d68a792e9386f07f65
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-05-24 00:13:55 +0530

    Add firebase user state change observer

--- js/app.js
+++ js/app.js
@@ -176,3 +176,15 @@ signupBtn.addEventListener("click", (e) => {
         }
     }
 });
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        var uid = user.uid;
+        // ...
+    } else {
+        // User is signed out
+        // ...
+    }
+});
