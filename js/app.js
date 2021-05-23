384451d8b98a5b48ebb7f09c649cc722b1358d50
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-05-23 18:45:23 +0530

    Add basic elements to script

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("#form");
+const nameFieldDiv = document.querySelector('.display-none')
+const nameField = document.querySelector("#name");
+const emailField = document.querySelector("#email");
+const passwordField = document.querySelector("#password");
+const loginBtn = document.querySelector("#login");
+const signupBtn = document.querySelector("#signup");
