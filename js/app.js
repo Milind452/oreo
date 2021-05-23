2c6a6313563986ce2cb50aaf719cf8e0fee4e654
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-05-24 00:10:36 +0530

    Format script

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("#form");
-const nameFieldDiv = document.querySelector('.display-none')
+const nameFieldDiv = document.querySelector(".display-none");
 const nameField = document.querySelector("#name");
 const emailField = document.querySelector("#email");
 const passwordField = document.querySelector("#password");
@@ -16,72 +16,79 @@ IssueTracker.prototype = {
     retrieve: function () {
         var message = "";
         switch (this.issues.length) {
-        case 0:
-            // do nothing because message is already ""
-            break;
-        case 1:
-            message = "Please correct the following issue:\n" + this.issues[0];
-            break;
-        default:
-            message = "Please correct the following issues:\n" + this.issues.join("\n");
-            break;
+            case 0:
+                // do nothing because message is already ""
+                break;
+            case 1:
+                message =
+                    "Please correct the following issue:\n" + this.issues[0];
+                break;
+            default:
+                message =
+                    "Please correct the following issues:\n" +
+                    this.issues.join("\n");
+                break;
         }
         return message;
-    }
+    },
 };
 
 function checkNameRequirements(name, nameIssueTracker) {
-    if(!name.match(/^[a-zA-Z]+$/g)) {
-        nameIssueTracker.add("enter valid name")
+    if (!name.match(/^[a-zA-Z]+$/g)) {
+        nameIssueTracker.add("enter valid name");
     }
 }
 
 function checkEmailRequirements(email, emailIssueTracker) {
-    if(!email.match(/[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+/g)) {
-        emailIssueTracker.add("enter valid email id")
+    if (!email.match(/[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+/g)) {
+        emailIssueTracker.add("enter valid email id");
     }
 }
 
 function checkPasswordRequirements(password, passwordIssueTracker) {
     if (password.length < 8) {
-      passwordIssueTracker.add("fewer than 8 characters");
+        passwordIssueTracker.add("fewer than 8 characters");
     } else if (password.length > 100) {
-      passwordIssueTracker.add("greater than 100 characters");
+        passwordIssueTracker.add("greater than 100 characters");
     }
 
     if (!password.match(/[\!\@\#\$\%\^\&\*]/g)) {
-      passwordIssueTracker.add("missing a symbol (!, @, #, $, %, ^, &, *)");
+        passwordIssueTracker.add("missing a symbol (!, @, #, $, %, ^, &, *)");
     }
 
     if (!password.match(/\d/g)) {
-      passwordIssueTracker.add("missing a number");
+        passwordIssueTracker.add("missing a number");
     }
 
     if (!password.match(/[a-z]/g)) {
-      passwordIssueTracker.add("missing a lowercase letter");
+        passwordIssueTracker.add("missing a lowercase letter");
     }
 
     if (!password.match(/[A-Z]/g)) {
-      passwordIssueTracker.add("missing an uppercase letter");
+        passwordIssueTracker.add("missing an uppercase letter");
     }
 
-    const illegalCharacterGroup = password.match(/[^A-z0-9\!\@\#\$\%\^\&\*]/g)
+    const illegalCharacterGroup = password.match(/[^A-z0-9\!\@\#\$\%\^\&\*]/g);
     if (illegalCharacterGroup) {
-      illegalCharacterGroup.forEach(function (illegalChar) {
-        passwordIssueTracker.add("includes illegal character: " + illegalChar);
-      });
+        illegalCharacterGroup.forEach(function (illegalChar) {
+            passwordIssueTracker.add(
+                "includes illegal character: " + illegalChar
+            );
+        });
     }
 }
 
-loginBtn.addEventListener('click', e => {
+loginBtn.addEventListener("click", (e) => {
     // e.preventDefault();
     const nameFieldClassList = nameFieldDiv.classList;
-    if(!nameFieldClassList.contains('display-none')) {
-        nameFieldClassList.add('display-none')
+    if (!nameFieldClassList.contains("display-none")) {
+        nameFieldClassList.add("display-none");
         signupBtn.parentElement.remove();
-        document.querySelector('.box__fields').insertAdjacentElement('beforeend', signupBtn.parentElement);
-        loginBtn.style.width = '300px';
-        signupBtn.style.width = '250px';
+        document
+            .querySelector(".box__fields")
+            .insertAdjacentElement("beforeend", signupBtn.parentElement);
+        loginBtn.style.width = "300px";
+        signupBtn.style.width = "250px";
         nameField.required = false;
     } else {
         const email = emailField.value;
@@ -92,25 +99,27 @@ loginBtn.addEventListener('click', e => {
         checkPasswordRequirements(password, passwordIssueTracker);
         const emailIssues = emailIssueTracker.retrieve();
         const passwordIssues = passwordIssueTracker.retrieve();
-        emailField.setCustomValidity(emailIssues)
-        passwordField.setCustomValidity(passwordIssues)
+        emailField.setCustomValidity(emailIssues);
+        passwordField.setCustomValidity(passwordIssues);
         if (passwordIssues.length === 0 && emailIssues.length == 0) {
-            console.log("LOGIN")
-            console.log("Email: " + email)
-            console.log("Password: " + password)
+            console.log("LOGIN");
+            console.log("Email: " + email);
+            console.log("Password: " + password);
         }
     }
-})
+});
 
-signupBtn.addEventListener('click', e => {
+signupBtn.addEventListener("click", (e) => {
     // e.preventDefault();
-    const nameFieldClassList = nameFieldDiv.classList
-    if(nameFieldClassList.contains('display-none')) {
-        nameFieldClassList.remove('display-none');
+    const nameFieldClassList = nameFieldDiv.classList;
+    if (nameFieldClassList.contains("display-none")) {
+        nameFieldClassList.remove("display-none");
         loginBtn.parentElement.remove();
-        document.querySelector('.box__fields').insertAdjacentElement('beforeend', loginBtn.parentElement);
-        loginBtn.style.width = '250px';
-        signupBtn.style.width = '300px';
+        document
+            .querySelector(".box__fields")
+            .insertAdjacentElement("beforeend", loginBtn.parentElement);
+        loginBtn.style.width = "250px";
+        signupBtn.style.width = "300px";
         nameField.required = true;
     } else {
         const name = nameField.value;
@@ -128,11 +137,15 @@ signupBtn.addEventListener('click', e => {
         nameField.setCustomValidity(nameIssues);
         emailField.setCustomValidity(emailIssues);
         passwordField.setCustomValidity(passwordIssues);
-        if (passwordIssues.length === 0 && emailIssues.length === 0 && nameIssues.length === 0) {
-            console.log('SIGNUP')
-            console.log("Name: " + name)
-            console.log("Email: " + email)
-            console.log("Password: " + password)
+        if (
+            passwordIssues.length === 0 &&
+            emailIssues.length === 0 &&
+            nameIssues.length === 0
+        ) {
+            console.log("SIGNUP");
+            console.log("Name: " + name);
+            console.log("Email: " + email);
+            console.log("Password: " + password);
         }
     }
-})
+});
