c263f942a63b9eaafb31ed7e73a580729a2849b6
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-05-24 20:19:29 +0530

    Add firebase auth state change observer and return to login page on logout

--- js/home.js
+++ js/home.js
@@ -8,9 +8,24 @@ logoutBtn.addEventListener("click", (e) => {
         .then(() => {
             // Sign-out successful.
             console.log("Successful logout");
+            window.location.href = "../index.html";
         })
         .catch((error) => {
             // An error happened.
             console.log("Logout failed");
         });
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        var uid = user.uid;
+        console.log(user);
+        // ...
+    } else {
+        // User is signed out
+        // ...
+        console.log("not logged in");
+    }
 });
